cc055756090aead2a710419faa4f976a120e25c3
Xinyi <xinzwerkz@gmail.com>
2021-08-25 18:26:52 +0800

    Add header image

--- instagram-react-clone/src/App.js
+++ instagram-react-clone/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* Header */}
+      <div className="app__header">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          alt=""
+          height="100px"
+          width="300px"
+          className="app__headerImage"
+        />
+      </div>
+      {/* Posts */}
+      {/* Posts */}
+    </div>
+  );
 }
 
 export default App;

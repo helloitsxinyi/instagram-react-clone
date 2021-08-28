64a75fb8bf222f7b7f79bcb89a9dac20034c5fae
Xinyi <xinzwerkz@gmail.com>
2021-08-28 18:08:46 +0800

    Complete post component and styling

--- instagram-react-clone/src/Post.js
+++ instagram-react-clone/src/Post.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Post.css";
+import { Avatar } from "@material-ui/core";
 
 function Post() {
   return (
     <div>
       <div className="post">
-        <h3>Username</h3>
+        <div className="post__header">
+          <Avatar
+            className="post__avatar"
+            alt="xinz"
+            src="/static/images/avatar/1.jpg"
+          />
+          <h3>Username</h3>
+        </div>
         {/* header: avatar + username */}
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg1MndL-Xp1JcnqaB0YOqTp6zDjrwYyGKsPA&usqp=CAU"
